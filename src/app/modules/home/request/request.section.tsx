c071fc00cbaab59f40e451f269d123739cacaf20
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2022-01-28 21:02:28 -0300

    feat: whatsapp links

--- src/app/modules/home/request/request.section.tsx
+++ src/app/modules/home/request/request.section.tsx
@@ -1,12 +1,9 @@
 import { Row, Col } from "react-flexbox-grid/dist/react-flexbox-grid";
 import { Grid, OnlyDesktop } from "atomic/obj.grid/grid";
 import { Separator } from "@atomic/atm.separator/separator.style";
-import { RequestForm } from "@app/modules/home/request/request-form.component";
 import { Body, H2, Enhance, LinkStyled } from "atomic/atm.typography/typography";
 import React from "react";
-import { ImageIcon, ImageObj } from "@atomic/atm.image";
-import { Hbox } from "@atomic/obj.hbox";
-import { ExternalLinks } from "@app/modules/app/external-links";
+import { Button } from "@atomic/atm.button";
 
 export const RequestSection: React.FC = () => (
   <Grid>
@@ -15,10 +12,10 @@ export const RequestSection: React.FC = () => (
     </OnlyDesktop>
     <Row center ={'xs'}>
       <Col xs={12} lg={6}>
-        <H2>Estamos em nosso <Enhance>pré-lançamento</Enhance>!</H2>
-        <Body>Faltam <Enhance>poucos dias</Enhance> para começarmos a entregar. Ganhe <Enhance>10% de desconto</Enhance> em sua primeira compra ao preencher seu email!</Body>
+        <H2>Ganhe <Enhance>30% de desconto</Enhance> em sua primeira compra!</H2>
+        <Body>Tá esperando o que pra ser <Enhance>saudável</Enhance>?</Body>
         <Separator type={"subsection"} />
-        <RequestForm />
+        <Button kind={"callToAction"} href={"https://wa.me/5511934622206?text=Olá!%20Quero%20realizar%20uma%20compra."}>Experimente agora!</Button>
       </Col>
     </Row>
     <Separator type={"section"} />
